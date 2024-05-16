05ac43900837c1c80e3932c5c5f4b622becbc867
Zheyu <roboticist.zheyu@gmail.com>
2024-05-16 15:08:30 +0200

    cleaned up saga, removed unused functions

--- robosaga/robosaga/saliency_guided_augmentation.py
+++ robosaga/robosaga/saliency_guided_augmentation.py
@@ -132,7 +132,7 @@ class SaliencyGuidedAugmentation:
             bg = obs_meta["randomisers"][i].forward_in(self.backgrounds[rand_bg_idx])
             bg = self.normalizer[obs_key].normalize(bg) if self.normalizer is not None else bg
             if self.aug_strategy == "saga_mixup":
-                smaps = torch.clip(smaps, 0, 0.5)
+                smaps = torch.clip(smaps, 0, 0.8)
                 x_aug = obs_dict[obs_key][aug_inds] * smaps + bg * (1 - smaps)
             elif self.aug_strategy == "saga_erase":
                 smaps[smaps < self.erase_thresh] = 0
