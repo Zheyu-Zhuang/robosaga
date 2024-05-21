7b58a493956a978830dd6a93e53d854af1a306a8
Zheyu <roboticist.zheyu@gmail.com>
2024-05-21 17:26:44 +0200

    updated distractor addition logic

---
+++ robomimic/robomimic/scripts/eval_saga_bc_rnn.py
@@ -0,0 +1,52 @@
+import os
+
+import numpy as np
+
+experiment_path = "../experiments/robosaga/robomimic/bc_rnn/bc_rnn_square_ph_saga_mixup_0.5_aug_0.5_clip/20240520204616/models"
+
+
+checkpoints = {
+    "280": 0.82,
+    "460": 0.84,
+    "540": 0.84,
+}
+
+
+def find_checkpoint_by_name(checkpoints, experiment_path):
+    checkpoints_ = []
+    all_files = os.listdir(experiment_path)
+    for epoch in checkpoints.keys():
+        for file in all_files:
+            if f"epoch_{epoch}" in file:
+                checkpoints_.append(file)
+                break
+
+    return checkpoints_
+
+
+checkpoint_paths = find_checkpoint_by_name(checkpoints, experiment_path)
+checkpoint_paths = [os.path.join(experiment_path, ckpt) for ckpt in checkpoint_paths]
+
+
+def get_average_success_states(checkpoints):
+    success_rate = [checkpoints[ckpt] for ckpt in checkpoints]
+    avg = np.mean(success_rate)
+    std = np.std(success_rate)
+    print(f"Average success rate: {avg:.2f} +/- {std:.2f}")
+
+
+get_average_success_states(checkpoints)
+
+n_rollouts = 50
+texture = "null"
+distractors = "bottle lemon milk can"
+
+commands = []
+for ckpt_path in checkpoint_paths:
+    commands.append(
+        f"python robomimic/scripts/eval_trained_agent.py --n_rollout 50 --agent {ckpt_path} --n_rollout {n_rollouts}  --distractors {distractors} --video_path ../{ckpt_path.split('/')[-1].split('.')[0]}.mp4"
+    )
+
+terminal_commands = " & ".join(commands)
+
+os.system(terminal_commands)
