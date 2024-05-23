89015ac339917ba1da7571025c140afd08152b61
Zheyu <roboticist.zheyu@gmail.com>
2024-05-23 18:05:36 +0200

    be quick

--- robomimic/robomimic/scripts/offdomain_eval_top3.py
+++ robomimic/robomimic/scripts/offdomain_eval_top3.py
@@ -24,10 +24,10 @@ def run_script(script_name, script_args, output_file):
 
         # Write the outputs to the file
         f.write(f"Output from {script_name} with arguments {' '.join(script_args)}:\n{output}\n")
-        if errors:
-            f.write(
-                f"Errors from {script_name} with arguments {' '.join(script_args)}:\n{errors}\n"
-            )
+        # if errors:
+        #     f.write(
+        #         f"Errors from {script_name} with arguments {' '.join(script_args)}:\n{errors}\n"
+        #     )
 
         # Print the output and errors to the terminal
         print(f"Output from {script_name} with arguments {' '.join(script_args)}:\n{output}\n")
@@ -114,23 +114,42 @@ if __name__ == "__main__":
                     + distractors,
                 )
             )
-    output_file = os.path.join(args.exp_path, f"logs/eval_results_{args.mode}.txt")
+
+    if args.mode == "texture":
+        output_file = os.path.join(args.exp_path, f"logs/eval_results_{texture}.txt")
+    else:
+        output_file = os.path.join(args.exp_path, f"logs/eval_results_{distractors}.txt")
 
     # Execute each script with its arguments and save the output
-    for script_name, script_args in scripts_with_args:
-        print(f"Running script {script_name} with arguments {script_args}")
-        run_script(script_name, script_args, output_file)
+    if os.path.exists(output_file):
+        overwrite = input(f"Output file {output_file} already exists. Overwrite? (y/n): ")
+        if overwrite.lower() == "n":
+            print("Computing Stats from existing output file")
+        else:
+            print("Rerun Evaluation")
+            os.remove(output_file)
+            for script_name, script_args in scripts_with_args:
+                print(f"Running script {script_name} with arguments {script_args}")
+                run_script(script_name, script_args, output_file)
+    else:
+        for script_name, script_args in scripts_with_args:
+            print(f"Running script {script_name} with arguments {script_args}")
+            run_script(script_name, script_args, output_file)
 
     # Extract success rates from the output file
     success_rates = extract_success_rates(output_file)
 
     print(f"\n===== Results for {args.exp_path} =====\n")
-    print(
-        f"In-domain Success rates: {np.mean(top_n_success_rate)} +/- {np.std(top_n_success_rate)}"
-    )
+    indomain_ssr_mean = np.around(np.mean(top_n_success_rate), 2)
+    indomain_ssr_std = np.around(np.std(top_n_success_rate), 2)
+    print(f"Indomain: {top_n_success_rate}, {indomain_ssr_mean} +/- {indomain_ssr_std}")
+    offdomain_ssr_mean = np.around(np.mean(success_rates), 2)
+    offdomain_ssr_std = np.around(np.std(success_rates), 2)
     if args.mode == "texture":
-        print(f"Off-domain for {texture}: {np.mean(success_rates)} +/- {np.std(success_rates)}")
+        print(
+            f"Off-domain for {texture}: {success_rates}, {offdomain_ssr_mean} +/- {offdomain_ssr_std}"
+        )
     else:
         print(
-            f"Off-domain for {distractors}: {np.mean(success_rates)} +/- {np.std(success_rates)}"
+            f"Off-domain for {distractors}: {success_rates}, {offdomain_ssr_mean} +/- {offdomain_ssr_std}"
         )
