7eb88aa7f07739cba892d6b6e48a7e8c3be01062
Zheyu Zhuang <zheyuzh@kth.se>
2024-05-31 20:08:40 +0200

    exps

--- dp_shit.py
+++ dp_shit.py
@@ -7,6 +7,8 @@ import dill
 import zarr
 import numpy
 
+from diffusion_policy.dataset.real_image_dataset import real_data_to_replay_buffer
+
 # # load checkpoint
 # ckpt_path = "/home/x_zhzhu/RoboSaGA/experiments/robosaga/archive/green_pnp_real/diffusion_policy/baseline/checkpoints/epoch=0550-train_loss=0.004.ckpt"
 # payload = torch.load(open(ckpt_path, 'rb'), pickle_module=dill)
@@ -35,63 +37,10 @@ import numpy
 
 # 
 data_path = 'data/green_pnp/replay_buffer.zarr'
-dataset = zarr.open(data_path, mode='w')
-
-data = dataset.create_group('data')
-meta = dataset.create_group('meta')
-
+store = zarr.DirectoryStore(data_path)
+src_root = zarr.group(store)
 #
-print(meta.keys())
+data = src_root['data']
+meta = src_root['meta']
 
-episode_ends = meta['episode_ends'][:]
-
-
-###
-channel = 'ee_rgb_camera'
-print(f"Creating {channel} array")
-data[channel] = data.create_dataset(
-    name=channel,
-    shape=(episode_ends[-1],) + obs_dict[channel].shape[1:],
-    dtype=obs_dict[channel].numpy().dtype,
-    # Chunk the data by episode
-    chunks=(int(episode_ends[-1] / len(episode_ends)),) + obs_dict[channel].shape[1:]
-)
-
-channel = 'front_rgb_camera'
-print(f"Creating {channel} array")
-data[channel] = data.create_dataset(
-    name=channel,
-    shape=(episode_ends[-1],) + obs_dict[channel].shape[1:],
-    dtype=obs_dict[channel].numpy().dtype,
-    # Chunk the data by episode
-    chunks=(int(episode_ends[-1] / len(episode_ends)),) + obs_dict[channel].shape[1:]
-)
-
-channel = 'robot_state'
-print(f"Creating {channel} array")
-data[channel] = data.create_dataset(
-    name=channel,
-    shape=(episode_ends[-1],) + obs_dict[channel].shape[1:],
-    dtype=obs_dict[channel].numpy().dtype,
-    # Chunk the data by episode
-    chunks=(int(episode_ends[-1] / len(episode_ends)),) + obs_dict[channel].shape[1:]
-)
-    
-# Action
-channel = 'ee_vel'
-data['action'] = data.create_dataset(
-    name='action',
-    shape=(episode_ends[-1], 7), # Add 7 for the gripper
-    dtype=action_dict[channel].numpy().dtype,
-    chunks=(int(episode_ends[-1] / len(episode_ends)),) + action_dict[channel].shape[1:]
-)
-    
-# Create the meta arrays
-meta['episode_ends'] = meta.create_dataset(
-    name='episode_ends',
-    shape=(len(episode_ends),),
-    dtype=np.array(episode_ends).dtype,
-    chunks=(1,),
-)
-
-
+print(data.tree())
